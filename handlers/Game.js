3fa754da9641b70a623fcd0a7164f4551e27c763
michael seaman <mseaman26@gmail.com>
2024-02-28 13:47:44 -0800

    properties on the game object for dealer and blinds, etc

--- handlers/Game.js
+++ handlers/Game.js
@@ -4,10 +4,20 @@ export class Game{
         this.players = players;
         this.turn = 0;
         this.active = false;
+        this.pot = 0;
+        this.dealer = 0;
+        this.dealerId = this.players[this.dealer];
+        this.smallBlindId = this.players[(this.dealer + 1) % this.players.length];
+        this.bigBlindId = this.players[(this.dealer + 2) % this.players.length];
+        this.round = 0;
     }
     nextTurn(){
         console.log('players.length: ', this.players.length)
         this.turn = (this.turn + 1) % this.players.length;
+    }
+    nextHand(){
+        this.dealer = (this.dealer + 1) % this.players.length;
+        this.turn = (this.dealer + 1) % this.players.length;
     }
     getCurrentPlayerId(){
         return this.players[this.turn];
