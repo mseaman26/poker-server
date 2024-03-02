013165a2e9e8485e2b5d9e99914fbd9fd6ac101c
michael seaman <mseaman26@gmail.com>
2024-03-02 13:46:58 -0800

    handling for blinds and chips

--- handlers/Game.js
+++ handlers/Game.js
@@ -1,15 +1,17 @@
 export class Game{
-    constructor(roomId, players = []){
+    constructor(roomId, players = [], dealer = 0, bigBlind){
         this.roomId = roomId;
         this.players = players;
-        this.turn = 0;
         this.active = false;
         this.pot = 0;
-        this.dealer = 0;
+        this.dealer = dealer;
         this.dealerId = this.players[this.dealer];
         this.smallBlindId = this.players[(this.dealer + 1) % this.players.length];
         this.bigBlindId = this.players[(this.dealer + 2) % this.players.length];
         this.round = 0;
+        this.turn = (this.dealer + 3) % this.players.length;
+        this.bigBlind = bigBlind
+        
     }
     nextTurn(){
         console.log('players.length: ', this.players.length)
@@ -17,7 +19,7 @@ export class Game{
     }
     nextHand(){
         this.dealer = (this.dealer + 1) % this.players.length;
-        this.turn = (this.dealer + 1) % this.players.length;
+        this.turn = (this.dealer + 3) % this.players.length;
     }
     getCurrentPlayerId(){
         return this.players[this.turn];
--- handlers/gameHandlers.js
+++ handlers/gameHandlers.js
@@ -3,15 +3,20 @@ const activeGames = new Map();
 
 export function handleGameEvents (io, socket){
     socket.on('start game', (data) => {
-        console.log('players: ', data.players)
+        console.log('start game data: ', data)
         io.to(data.roomId).emit('game started');
         if(!activeGames.has(data.roomId)){
-            activeGames.set(data.roomId, new Game(data.roomId, data.players));
+            activeGames.set(data.roomId, new Game(data.roomId, data.players, data.dealer, data.bigBlind));
         }
         activeGames.get(data.roomId).active = true;
+        let players = activeGames.get(data.roomId).players;
+        for(let i = 0; i < players.length; i++){
+            players[i].chips = data.buyIn;
+        }
         console.log('active games: ', activeGames);
         console.log('game: ', activeGames.get(data.roomId));
         console.log('players: ', activeGames.get(data.roomId).players);
+        io.to(data.roomId).emit('start game', activeGames.get(data.roomId));
         io.to(data.roomId).emit('game state', activeGames.get(data.roomId));
     });
     socket.on('game state', (roomId) => {
@@ -32,11 +37,13 @@ export function handleGameEvents (io, socket){
         io.to(data.roomId).emit('game state', activeGames.get(data.roomId));
 
     });
-    socket.on('next hand', async (roomId) => {
+    socket.on('next hand', async (roomId, callback) => {
         console.log('roomId: ', roomId)
         console.log('actve games: ', activeGames)   
         await activeGames.get(roomId).nextHand();
+        io.to(roomId).emit('next hand', activeGames.get(roomId));
         io.to(roomId).emit('game state', activeGames.get(roomId));
+        callback();
 
     });
     socket.on('end game', (roomId) => {
